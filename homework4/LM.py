a9729d6fbefe9a4b4a2c93a0d7db69a10ae87d94
丁稼宇 <1501793890@qq.com>
2021-11-18 22:53:40 +0800

    LM

--- homework4/LM.py
+++ homework4/LM.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 LM = loadmat(r"data/LM.mat")
 alpha = LM['alpha_res'].squeeze()
 iter = LM['iter_res'].squeeze()
-plt.plot(alpha[1:], iter[1:])
+plt.plot(iter[1:], alpha[1:])
 plt.show()
