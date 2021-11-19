2a05c2316006d3d6d5ac12d4e55c982b4ddec187
nachewigkeit <1501793890@qq.com>
2021-11-20 00:28:25 +0800

    visualize

--- homework4/LM.py
+++ homework4/LM.py
@@ -4,5 +4,8 @@ import matplotlib.pyplot as plt
 LM = loadmat(r"data/LM.mat")
 alpha = LM['alpha_res'].squeeze()
 iter = LM['iter_res'].squeeze()
-plt.plot(iter[1:], alpha[1:])
-plt.show()
+
+plt.xlabel("alpha", fontsize=12)
+plt.ylabel("iter", fontsize=12)
+plt.plot(alpha[1:], iter[1:])
+plt.savefig(r"data/result/alpha.png", bbox_inches='tight')
--- homework4/calib.py
+++ homework4/calib.py
@@ -1,6 +1,7 @@
 import glob
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 # 获取标定板角点的位置
 shape = (7, 11)
@@ -23,11 +24,9 @@ for fname in images:
         corners2 = cv2.cornerSubPix(gray, corners, (5, 5), (-1, -1), criteria)
         img_points.append(corners2)
 
-        '''
         cv2.drawChessboardCorners(img, shape, corners, ret)
-        cv2.imshow('img', img)
-        cv2.waitKey(-1)
-        '''
+        plt.imshow(img)
+        plt.savefig(r"data/result/corner.png", bbox_inches='tight')
     else:
         print(fname)
 
@@ -36,22 +35,3 @@ ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, size,
 print("ret:", ret)
 print("mtx:\n", mtx)  # 内参数矩阵
 print("dist:\n", dist)  # 畸变系数   distortion cofficients = (k_1,k_2,p_1,p_2,k_3)
-
-img = cv2.imread(r"image/001.bmp")
-h, w = img.shape[:2]
-newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
-dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
-x, y, w, h = roi
-dst = dst[y:y + h, x:x + w]
-
-mean_error = 0
-
-for j in range(5):
-    dist_new = dist.copy()
-    dist_new[0, j] = 0
-    print(dist_new)
-    for i in range(len(obj_points)):
-        img_points2, _ = cv2.projectPoints(obj_points[i], rvecs[i], tvecs[i], mtx, dist_new)
-        error = cv2.norm(img_points[i], img_points2, cv2.NORM_L2)
-        mean_error += error
-    print(error / len(img_points))
